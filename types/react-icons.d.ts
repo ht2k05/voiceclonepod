d3e586565ed23ab8b76a2e6f6ed1c53e3d3b8d91
ht2k05 <your.email@example.com>
2024-09-27 21:07:24 -0400

    Initial commit of VoiceClone Podcast Creator1

--- types/react-icons.d.ts
+++ types/react-icons.d.ts
@@ -5,4 +5,9 @@ declare module 'react-icons/fa' {
   export const FaSpinner: IconType;
   export const FaPlay: IconType;
   export const FaPause: IconType;
+  export const FaBackward: IconType;
+  export const FaForward: IconType;
+  export const FaVolumeUp: IconType;
+  export const FaVolumeMute: IconType;
+  export const FaDownload: IconType;
 }
